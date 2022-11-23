3e910e94481db0919c1d790701c96da0a9921653
grinakate <89151831+grinakate@users.noreply.github.com>
2022-11-23 22:44:43 +0300

    task2: added bucket_sort

---
+++ Лабораторная работа №4/task2/bucket_sort.py
@@ -0,0 +1,34 @@
+def bucket_sort(array):
+    if len(array) <= 1:
+        return array
+
+    max_value = max(array)
+    min_value = min(array)
+
+    if min_value == max_value:
+        return array
+
+    length = len(array)
+    size = (max_value - min_value) / length
+
+    buckets = [[] for _ in range(length)]
+
+    for i in range(length):
+        index = int((array[i] - min_value) / size)
+        if index != length:
+            buckets[index].append(array[i])
+        else:
+            buckets[index - 1].append(array[i])
+
+    for i in range(len(array)):
+        buckets[i] = bucket_sort(buckets[i])
+
+    result = []
+    for i in range(length):
+        result = result + buckets[i]
+
+    return result
+
+
+array = [float(i) for i in input("Введите список чисел через пробел: ").split()]
+print(bucket_sort(array))
