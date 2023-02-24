9fa81a4eb1f2d30958b4f990771bf0a5e041f662
yanaz <jdbsjrv@gmail.com>
2023-02-24 20:09:27 +0300

    hihih

--- дз1/1.py
+++ дз1/1.py
@@ -1,3 +1,4 @@
+from timeit import default_timer
 
 fibonacci = [0, 1]
 for i in range(2, 500):
@@ -8,6 +9,7 @@ fib_str = ''.join(fibonacci)
 
 count = {str(i) + str(j) : 0 for i in range(10) for j in range(10)}
 
+t = default_timer()
 for i in range(len(fib_str) - 1):
     count[fib_str[i:i+2]] += 1
 
@@ -15,4 +17,6 @@ count_values = list(count.values())
 count_keys = list(count.keys())
 max_count = max(count_values)
 ind = count_values.index(max_count)
+t_search = default_timer() - t
 print(count_keys[ind], max_count)
+print("Время поиска:", t_search)
