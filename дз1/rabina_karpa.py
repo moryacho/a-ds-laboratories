fc3ffa0169fa36b27a0f28efed10dd0fbb22a599
yanaz <jdbsjrv@gmail.com>
2023-03-14 11:45:57 +0300

    fixed robina karpa

--- дз1/rabina_karpa.py
+++ дз1/rabina_karpa.py
@@ -1,18 +1,18 @@
-def get_hash(text: str):
-    X = 2
+def get_hash(text: str, len_pattern: int):
+    X = 10
     result = 0
     for i in range(len(text)):
-        result += int(text[i]) * (X ** (i % 2))
+        result += int(text[i]) * (X ** (len_pattern - i))
     return result
 
 
 def rabina_karpa(text: str, pattern: str):
     pattern_len = len(pattern)
     text_len = len(text)
-    pattern_hash = get_hash(pattern)
+    pattern_hash = get_hash(pattern, pattern_len)
     count = 0
     for i in range(text_len - pattern_len + 1):
-        text_hash = get_hash(text[i: i + pattern_len])
+        text_hash = get_hash(text[i: i + pattern_len], pattern_len)
         if pattern_hash == text_hash and pattern == text[i: i + pattern_len]:
             count += 1
 
