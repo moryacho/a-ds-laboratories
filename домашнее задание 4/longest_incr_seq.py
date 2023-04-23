42f8f20a511b4f757406a7447a68f563ce695a82
grinakate <89151831+grinakate@users.noreply.github.com>
2023-04-23 20:35:46 +0300

    динамическое программирование: задание 3

--- домашнее задание 4/longest_incr_seq.py
+++ домашнее задание 4/longest_incr_seq.py
@@ -4,13 +4,13 @@ n = int(input("Введите длину массива: "))
 N = [randint(-100, 100) for _ in range(n)]
 print(*["{:4d}".format(i) for i in N])
 
-N2 = [0 for _ in range(n)]
-N2[0] = 1
+sequence_numbers = [0 for _ in range(n)]
+sequence_numbers[0] = 1
 for i in range(1, n):
     if N[i] > N[i - 1]:
-        N2[i] = N2[i - 1] + 1
+        sequence_numbers[i] = sequence_numbers[i - 1] + 1
     else:
-        N2[i] = 1
+        sequence_numbers[i] = 1
 
-print(*["{:4d}".format(i) for i in N2])
-print(f"Длина наибольшей непрерывной строго возрастающей последовательности: {max(N2)}")
+print(*["{:4d}".format(i) for i in sequence_numbers])
+print(f"Длина наибольшей непрерывной строго возрастающей последовательности: {max(sequence_numbers)}")
